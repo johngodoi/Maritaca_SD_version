49a3df6392aac7fffe7488960fc187ecd52307d7
emigueltriana <emigueltriana@8ef2b3f4-a850-4f10-94f6-df0e84ed724b>
2011-11-03 19:37:03 +0000

    test webservice in proposed folder structure
    git-svn-id: svn+ssh://svn.code.sf.net/p/maritaca/code/trunk/server@6 8ef2b3f4-a850-4f10-94f6-df0e84ed724b

---
+++ core/src/test/java/br/unifesp/maritaca/core/QuestionnaireTest.java
@@ -0,0 +1,14 @@
+package br.unifesp.maritaca.core;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class QuestionnaireTest {
+
+	@Test
+	public void test() {
+		assertEquals(true, true);
+	}
+
+}
