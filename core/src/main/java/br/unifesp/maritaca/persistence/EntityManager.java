ad7c027f9a1569923bf4c8205b10145d1b495aa6
emigueltriana <emigueltriana@8ef2b3f4-a850-4f10-94f6-df0e84ed724b>
2011-11-23 21:25:39 +0000

    RESTful services for Answer \ Renamed Response to Answer in Maritaca to avoid ambiguity \ Changed exception handling of all application
    git-svn-id: svn+ssh://svn.code.sf.net/p/maritaca/code/trunk/server@19 8ef2b3f4-a850-4f10-94f6-df0e84ed724b

--- core/src/main/java/br/unifesp/maritaca/persistence/EntityManager.java
+++ core/src/main/java/br/unifesp/maritaca/persistence/EntityManager.java
@@ -1,46 +1,26 @@
 package br.unifesp.maritaca.persistence;
 
-import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 import java.util.UUID;
 
 public interface EntityManager {
-	<T> boolean persist(T obj) throws IllegalArgumentException,
-			IllegalAccessException, SecurityException, NoSuchMethodException,
-			InvocationTargetException;
+	<T> boolean persist(T obj);
 
-	<T> boolean persist(T obj, boolean createTable)
-			throws IllegalArgumentException, IllegalAccessException,
-			SecurityException, NoSuchMethodException, InvocationTargetException;
+	<T> boolean persist(T obj, boolean createTable);
 
-	<T> T find(Class<T> cl, UUID uuid) throws InstantiationException,
-			IllegalAccessException, IllegalArgumentException,
-			SecurityException, InvocationTargetException,
-			NoSuchMethodException, NoSuchFieldException;
+	<T> T find(Class<T> cl, UUID uuid);
 
-	<T> boolean delete(T obj) throws IllegalArgumentException,
-			SecurityException, IllegalAccessException,
-			InvocationTargetException, NoSuchMethodException;
+	<T> boolean delete(T obj);
 
-	<T> List<T> cQuery(Class<T> cl, String field, String value)
-			throws IllegalArgumentException, SecurityException,
-			InstantiationException, IllegalAccessException,
-			InvocationTargetException, NoSuchMethodException,
-			NoSuchFieldException;
+	<T> List<T> cQuery(Class<T> cl, String field, String value);
 
-	<T> List<T> listAll(Class<T> cl) throws IllegalArgumentException,
-			SecurityException, InstantiationException, IllegalAccessException,
-			InvocationTargetException, NoSuchMethodException,
-			NoSuchFieldException;
+	<T> List<T> listAll(Class<T> cl);
 
-	<T> List<T> listAllMinimal(Class<T> cl) throws IllegalArgumentException,
-			SecurityException, InstantiationException, IllegalAccessException,
-			InvocationTargetException, NoSuchMethodException,
-			NoSuchFieldException;
+	<T> List<T> listAllMinimal(Class<T> cl);
 
-	<T> boolean createTable(Class<T> cl) throws Exception;
+	<T> boolean createTable(Class<T> cl);
 
-	<T> boolean tableExists(Class<T> cl) throws Exception;
+	<T> boolean tableExists(Class<T> cl);
 
-	<T> boolean dropTable(Class<T> cl) throws Exception;
+	<T> boolean dropTable(Class<T> cl);
 }
