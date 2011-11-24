4e9689defca32494e4ad2aa14a961f01c30f337a
emigueltriana <emigueltriana@8ef2b3f4-a850-4f10-94f6-df0e84ed724b>
2011-11-24 19:29:36 +0000

    refactored entitymanagerhector to improve source reuse \ added annotations to mark classes to be saved as column family in cassandra Added annotation @Minimal to let the user to request just minimal information of a column family and avoid download big data if it is not necessary\ Added index criation for entities that has the @Column(indexed=true) mark \ Some test renamed and improved
    git-svn-id: svn+ssh://svn.code.sf.net/p/maritaca/code/trunk@20 8ef2b3f4-a850-4f10-94f6-df0e84ed724b

---
+++ server/core/src/test/java/br/unifesp/maritaca/control/impl/FormAnswerCtrlImplTest.java
@@ -0,0 +1,112 @@
+package br.unifesp.maritaca.control.impl;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.UUID;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+
+import br.unifesp.maritaca.core.Form;
+import br.unifesp.maritaca.persistence.EntityManager;
+
+public class FormAnswerCtrlImplTest {
+	private static final String uuid = "637dea60-146e-11e1-a7c0-d2b70b6d4d67";
+	private static final String uuid2 = "537dea60-146e-11e1-a7c0-d2b70b6d4d67";
+//	private static final String uuid3 = "437dea60-146e-11e1-a7c0-d2b70b6d4d67";
+
+	private EntityManager em;
+	private FormAnswerCtrlImpl frControl;
+
+	@Before
+	public void setUp() throws Exception {
+		em = mock(EntityManager.class);
+		frControl = new FormAnswerCtrlImpl();
+		frControl.setEntityManager(em);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testSaveForm() {
+		Form form = new Form();
+
+		when(em.persist(form)).thenAnswer(new Answer<Boolean>() {
+
+			@Override
+			public Boolean answer(InvocationOnMock invocation) throws Throwable {
+				Form form = (Form) invocation.getArguments()[0];
+				form.setKey(uuid);
+				return true;
+			}
+		});
+		assertNull(form.getKey());
+		assertTrue(frControl.saveForm(form));
+		assertEquals(uuid, form.getKey().toString());
+	}
+
+	@Test
+	public void testGetForm() {
+		assertNull(frControl.getForm(null));
+
+		UUID uid = UUID.fromString(uuid);
+		Form form = new Form();
+		form.setKey(uid);
+		when(em.find(Form.class, uid)).thenReturn(form);
+		assertNotNull(frControl.getForm(uid));
+	}
+
+	@Test
+	public void testListAllForms() {
+		// dumb test while FormAnswerControl just redirect the request
+		// to entitymanager.listAll(Form.class)
+		Form form1 = new Form();
+		form1.setKey(uuid);
+		Form form2 = new Form();
+		form2.setKey(uuid2);
+		ArrayList<Form> list = new ArrayList<Form>();
+		list.add(form1);
+		list.add(form2);
+
+		when(em.listAll(Form.class)).thenReturn(list);
+
+		Collection<Form> lresult = frControl.listAllForms();
+		assertNotNull(lresult);
+
+		for (Form f : lresult) {
+			assertTrue(lresult.contains(f));
+		}
+	}
+
+	// Tests no finished because methods in
+	// FormAnswerControlImpl are just redirecting
+	// the request to the entitymanager
+
+	// @Test
+	// public void testListAllFormsMinimal(){}
+	//
+	// @Test
+	// public void testSaveAnswer(){}
+	//
+	// @Test
+	// public void testGetAnswer(){}
+	//
+	// @Test
+	// public void testlistAllAnswer(){}
+	//
+	// @Test
+	// public void testlistAllAnswerMinimal(){}
+
+}
--- server/core/src/test/java/br/unifesp/maritaca/persistence/EntityManagerHectorImplTest.java
+++ server/core/src/test/java/br/unifesp/maritaca/persistence/EntityManagerHectorImplTest.java
@@ -1,9 +1,9 @@
 package br.unifesp.maritaca.persistence;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Collection;
 
 import javax.annotation.Resource;
 
@@ -14,7 +14,7 @@ import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import br.unifesp.maritaca.util.CFTest;
+import br.unifesp.maritaca.util.CFforTesting;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "/cassandra-context-test-v2.xml")
@@ -25,27 +25,62 @@ public class EntityManagerHectorImplTest extends BaseEmbededServerSetupTest {
 
 	@Test
 	public void testCreateExistsDeleteColumnFamily() {
-		assertFalse(emHectorImpl.tableExists(CFTest.class));
-		assertTrue(emHectorImpl.createTable(CFTest.class));
-		assertTrue(emHectorImpl.tableExists(CFTest.class));
-		assertTrue(emHectorImpl.dropTable(CFTest.class));
-		assertFalse(emHectorImpl.tableExists(CFTest.class));
+		assertFalse(emHectorImpl.tableExists(CFforTesting.class));
+		assertTrue(emHectorImpl.createTable(CFforTesting.class));
+		assertTrue(emHectorImpl.tableExists(CFforTesting.class));
+		assertTrue(emHectorImpl.dropTable(CFforTesting.class));
+		assertFalse(emHectorImpl.tableExists(CFforTesting.class));
 
 	}
 
 	@Test
 	public void testInsertGetDelete() {
-		CFTest cfTest = new CFTest();
+		CFforTesting cfTest = new CFforTesting();
 		cfTest.setName("myname");
 
 		emHectorImpl.persist(cfTest);// create a CF if doesn't exists
-		CFTest cfTest2 = emHectorImpl.find(cfTest.getClass(), cfTest.getKey());
+		CFforTesting cfTest2 = emHectorImpl.find(cfTest.getClass(),
+				cfTest.getKey());
 		assertEquals(cfTest.getKey(), cfTest2.getKey());
 		assertEquals(cfTest.getName(), cfTest2.getName());
 		emHectorImpl.delete(cfTest);
 		cfTest = emHectorImpl.find(cfTest.getClass(), cfTest.getKey());
 		assertNull(cfTest);
+	}
 
+	@Test
+	public void testListAll() {
+		ArrayList<CFforTesting> mylist = new ArrayList<CFforTesting>();
+		CFforTesting cfTest = new CFforTesting();
+		cfTest.setName("myname");
+		cfTest.setBigData("bigdata");
+		emHectorImpl.persist(cfTest);
+		mylist.add(cfTest);
+
+		cfTest = new CFforTesting();
+		cfTest.setName("myname");
+		cfTest.setBigData("bigdata");
+		emHectorImpl.persist(cfTest);
+		mylist.add(cfTest);
+
+		Collection<CFforTesting> lresult = emHectorImpl
+				.listAll(CFforTesting.class);
+
+		assertNotNull(lresult);
+		assertEquals(mylist.size(), lresult.size());
+		for (CFforTesting cf : lresult) {
+			assertNotNull(cf.getBigData());
+		}
+
+		lresult = null;
+		// minimal data
+		lresult = emHectorImpl.listAll(CFforTesting.class, true);
+
+		assertNotNull(lresult);
+		assertEquals(mylist.size(), lresult.size());
+		for (CFforTesting cf : lresult) {
+			assertNull(cf.getBigData());
+		}
 	}
 
 }
