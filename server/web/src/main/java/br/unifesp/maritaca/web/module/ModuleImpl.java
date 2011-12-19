cc6ae15a3ef5a0a10411fafa1ba9bb5bd03fd362
emigueltriana <emigueltriana@8ef2b3f4-a850-4f10-94f6-df0e84ed724b>
2011-12-19 21:45:46 +0000

    Ticket:18 Status: closed NewForm and ViewForm submodules removed, now the EditFormBean has two boolean attributes to indicate if the form is editable or new
    git-svn-id: svn+ssh://svn.code.sf.net/p/maritaca/code/trunk@32 8ef2b3f4-a850-4f10-94f6-df0e84ed724b

--- server/web/src/main/java/br/unifesp/maritaca/web/module/ModuleImpl.java
+++ server/web/src/main/java/br/unifesp/maritaca/web/module/ModuleImpl.java
@@ -11,83 +11,83 @@ import br.unifesp.maritaca.web.utils.StringProcessing;
 
 public class ModuleImpl implements Module {
 
-    private String title;
-    private SubModule activeSubModule;
-    private List<SubModule> subModules;
+	private String title;
+	private SubModule activeSubModule;
+	private List<SubModule> subModules;
 
-    public ModuleImpl() {
-        subModules = new ArrayList<SubModule>();
-    }
+	public ModuleImpl() {
+		subModules = new ArrayList<SubModule>();
+	}
 
-    public String getTitle() {
-        return title;
-    }
+	public String getTitle() {
+		return title;
+	}
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
+	public void setTitle(String title) {
+		this.title = title;
+	}
 
-    public List<SubModule> getSubModules() {
-        return subModules;
-    }
+	public List<SubModule> getSubModules() {
+		return subModules;
+	}
 
-    public void setSubModules(List<SubModule> subModules) {
-        this.subModules = subModules;
-    }
+	public void setSubModules(List<SubModule> subModules) {
+		this.subModules = subModules;
+	}
 
-    @Override
-    public SubModule getActiveSubModule() {
-        if (activeSubModule == null) {
-            if (getSubModules() != null) {
-                return getSubModules().get(0);
-            }
-        }
-        return activeSubModule;
-    }
+	@Override
+	public SubModule getActiveSubModule() {
+		if (activeSubModule == null) {
+			if (getSubModules() != null) {
+				return getSubModules().get(0);
+			}
+		}
+		return activeSubModule;
+	}
 
-    @Override
-    public void setActiveSubModule(SubModule submod) {
-        if (submod != null) {
-            activeSubModule = submod;
-        }
-    }
+	@Override
+	public void setActiveSubModule(SubModule submod) {
+		if (submod != null) {
+			activeSubModule = submod;
+		}
+	}
 
-    @Override
-    public void addModule(SubModule submod) {
-        submod.setParent(this);
-        getSubModules().add(submod);
-    }
+	@Override
+	public void addModule(SubModule submod) {
+		submod.setParent(this);
+		getSubModules().add(submod);
+	}
 
-    @Override
-    public SubModule searchSubModule(String submodId) {
-        if (submodId != null) {
-            for (SubModule sm : getSubModules()) {
-                if (sm.getId().equals(submodId)) {
-                    return sm;
-                }
-            }
-        }
-        return null;
-    }
+	@Override
+	public SubModule searchSubModule(String submodId) {
+		if (submodId != null) {
+			for (SubModule sm : getSubModules()) {
+				if (sm.getId().equals(submodId)) {
+					return sm;
+				}
+			}
+		}
+		return null;
+	}
 
-    @Override
-    public void setActiveSubModuleByString(String submodId) {
-        setActiveSubModule(searchSubModule(submodId));
-    }
+	@Override
+	public void setActiveSubModuleByString(String submodId) {
+		setActiveSubModule(searchSubModule(submodId));
+	}
 
-    @Override
-    public String getId() {
-        return StringProcessing.getCompactedVersion(getTitle());
-    }
+	@Override
+	public String getId() {
+		return StringProcessing.getCompactedVersion(getTitle());
+	}
 
-    @Override
-    public void setId(String id) {
-        // dumb function
-    }
+	@Override
+	public void setId(String id) {
+		// dumb function
+	}
 
-    @Override
-    public void processAction(ActionEvent event)
-            throws AbortProcessingException {
-        setActiveSubModuleByString(event.getComponent().getId());
-    }
+	@Override
+	public void processAction(ActionEvent event)
+			throws AbortProcessingException {
+		setActiveSubModuleByString(event.getComponent().getId());
+	}
 }
