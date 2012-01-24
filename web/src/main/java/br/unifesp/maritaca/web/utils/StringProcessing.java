e562cc8d860c8c21b1b974af3ea6eb4dc60921a2
alvarohenry <alvarohenry@8ef2b3f4-a850-4f10-94f6-df0e84ed724b>
2012-01-24 18:48:10 +0000

    Ticket 28, status: closed Import a Form from a file or url to be load in Form Editor Utils class added
    git-svn-id: svn+ssh://svn.code.sf.net/p/maritaca/code/trunk/server@57 8ef2b3f4-a850-4f10-94f6-df0e84ed724b

--- web/src/main/java/br/unifesp/maritaca/web/utils/StringProcessing.java
+++
@@ -1,9 +0,0 @@
-package br.unifesp.maritaca.web.utils;
-
-public class StringProcessing {
-	public static final String SPACEWILDCARD="_";
-	public static String getCompactedVersion(String str){
-		if(str == null) return null;
-		return str.replaceAll(" ", SPACEWILDCARD);
-	}
-}
