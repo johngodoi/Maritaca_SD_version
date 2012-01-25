2e925f4b0f4febed82b54601182eabf6f8216574
emigueltriana <emigueltriana@8ef2b3f4-a850-4f10-94f6-df0e84ed724b>
2012-01-25 19:48:59 +0000

    No ticket - List form by User - Fixed error in web/pom.xml related with jersey
    git-svn-id: svn+ssh://svn.code.sf.net/p/maritaca/code/trunk/server@59 8ef2b3f4-a850-4f10-94f6-df0e84ed724b

--- web/src/main/java/br/unifesp/maritaca/web/jsf/form/ListFormsBean.java
+++ web/src/main/java/br/unifesp/maritaca/web/jsf/form/ListFormsBean.java
@@ -3,21 +3,26 @@ package br.unifesp.maritaca.web.jsf.form;
 import java.util.Collection;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
 
 import br.unifesp.maritaca.core.Form;
 import br.unifesp.maritaca.web.jsf.AbstractBean;
+import br.unifesp.maritaca.web.jsf.home.CurrentUserBean;
 
 @ManagedBean
 public class ListFormsBean extends AbstractBean {
 	private static final long serialVersionUID = 1L;
 	private Collection<Form> forms;
 	
+	@ManagedProperty("#{currentUserBean}")
+	private CurrentUserBean currentUser;
+	
 	public ListFormsBean() {
 		super(true, false);
 	}
 	
 	public Collection<Form> getForms() {
-		forms = formAnswCtrl.listAllFormsMinimal();
+		forms = formAnswCtrl.listAllFormsMinimalByUser(currentUser.getUser());
 		return forms;
 	}
 	public void setForms(Collection<Form> forms) {
@@ -26,5 +31,13 @@ public class ListFormsBean extends AbstractBean {
 	
 	public String viewForm(){
 		return "viewForm";
+	}
+
+	public CurrentUserBean getCurrentUser() {
+		return currentUser;
+	}
+
+	public void setCurrentUser(CurrentUserBean currentUser) {
+		this.currentUser = currentUser;
 	}
 }
