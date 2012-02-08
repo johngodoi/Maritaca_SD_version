c39b36faf1cf8f091293c16e649bb9ec4965d972
alvarohenry <alvarohenry@svn.code.sf.net>
2012-02-08 18:12:43 -0200

    Ticket # 51, status closed
    Implement the component date in the form editor.
    Fix the Field Text.
    Fix the required bug.

--- server/web/src/main/webapp/resources/javascript/h5FieldClasses.js
+++ server/web/src/main/webapp/resources/javascript/h5FieldClasses.js
@@ -49,15 +49,15 @@ var Field = function() {
 	this.showProperties = function(){
 		$('#fieldLabel').val(this.title);
 		$('#fieldHelp').val(this.help);
-		$('#fieldRequiredTrue:checked').val(this.required);
-		$('#fieldRequiredFalse:checked').val(!this.required);
+		$('#fieldRequiredTrue').prop('checked', this.required);
+		$('#fieldRequiredFalse').prop('checked', !this.required);
 		this.showSpecificProperties();
 	};
 	
 	this.saveProperties = function(){
 		this.title = $('#fieldLabel').val();
 		this.help = $('#fieldHelp').val();
-		this.required = $('#fieldRequiredTrue:checked').val() == 'on' ? true : false;
+		this.required = $('#fieldRequiredTrue').is(':checked');
 		this.saveSpecificProperties();
 	};
 };
@@ -141,20 +141,20 @@ var NumberField = function(){
 	};
 	
 	this.addXMLElements = function(){
-		//TODO: return IF comparisions
+		//TODO: return IF comparisons
 		return "";
 
 	};
 	
 	this.showSpecificProperties = function(){
-		$('#fieldDefault').val(this.bydefault).parent().show();
+		$('#fieldNumericDefault').val(this.bydefault).parent().show();
 		$('#fieldMaxValue').val(this.maxValue).parent().show();
-		$('#fieldMinValue').val(this.size).parent().show();
+		$('#fieldMinValue').val(this.minValue).parent().show();
 		$('#propertiesSpecific').show();
 	};
 	
 	this.saveSpecificProperties = function(){
-		this.bydefault = $('#fieldDefault').val();
+		this.bydefault = $('#fieldNumericDefault').val();
 		this.minValue = $('#fieldMinValue').val();
 		this.maxValue = $('#fieldMaxValue').val();
 	};
@@ -169,16 +169,71 @@ var NumberField = function(){
 };
 NumberField.prototype = new Field();
 
+var DateField = function() {
+	this.bydefault = '';
+	this.maxValue = '';
+	this.minValue = '';
+	
+	this.toHTMLSpecific = function() {
+		var html = '';
+		
+		html += '<input ';
+		html += attribCreator( 'type' , this.type);
+		html += attribCreator('id', 'field_' + this.id);
+		html += this.specificAttributes();
+		html += '/>';
+		
+		return html;
+	};
+	
+	this.addXMLSpecificAttributes = function() {
+		return this.specificAttributes();
+	};
+	
+	this.addXMLElements = function(){
+		//TODO: return IF comparisons
+		return "";
+
+	};
+	
+	this.showSpecificProperties = function(){
+		$('#fieldDateDefault').val(this.bydefault).parent().show();
+		$('#fieldMaxValueDate').val(this.maxValue).parent().show();
+		$('#fieldMinValueDate').val(this.minValue).parent().show();
+		$('#propertiesSpecific').show();
+	};
+
+	this.saveSpecificProperties = function(){
+		this.bydefault = $('#fieldDateDefault').val();
+		this.maxValue = $('#fieldMaxValueDate').val();
+		this.minValue = $('#fieldMinValueDate').val();
+	};
+
+	this.specificAttributes = function(){
+		var att = attribCreator('min', this.minValue);
+		att += attribCreator('max', this.maxValue);
+		if(this.bydefault)
+			att += attribCreator('value', this.bydefault);
+		return att;
+	};
+};
+DateField.prototype = new Field();
+
 var fieldFactory = function(type){
 	var field;
 	switch (type) {
 	case 'text':
 		field = new TextBox();
+		field.title = jQuery.i18n.prop('msg_form_textBoxTitle');
 		break;
 	case 'number':
 		field = new NumberField();
+		field.title = jQuery.i18n.prop('msg_form_numberTitle');
 		break;
-
+	case 'date':
+		field = new DateField();
+		field.title = jQuery.i18n.prop('msg_form_dateTitle');
+		break;
 	default:
 		return null;
 	}
