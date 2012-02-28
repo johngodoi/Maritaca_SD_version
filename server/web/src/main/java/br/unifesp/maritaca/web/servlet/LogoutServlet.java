eb9b28f85902bc51209377391ddf46bf4e8e2934
emigueltriana <edwintriana@gmail.com>
2012-02-28 11:49:13 -0300

    Ticket 67, status closed - Added filter to redirect the user to home.xhtml if he/she is authenticated - Bugfix: modified getServerName and getSchema in ShareFormBean

--- server/web/src/main/java/br/unifesp/maritaca/web/servlet/LogoutServlet.java
+++ server/web/src/main/java/br/unifesp/maritaca/web/servlet/LogoutServlet.java
@@ -15,14 +15,6 @@ public class LogoutServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	/**
-	 * @see HttpServlet#HttpServlet()
-	 */
-	public LogoutServlet() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-
-	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
 	 *      response)
 	 */
