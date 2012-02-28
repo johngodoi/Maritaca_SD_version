a8526a3d1f62919f586f5e433138a96a85c9e03c
alvarohenry <alvarohenry@svn.code.sf.net>
2012-02-28 17:12:07 -0300

    Ticket 66, status closed
    Size of Name and lastname: min=2 and max=20
    Email validation
    Password hidden

--- server/web/src/main/java/br/unifesp/maritaca/web/jsf/account/AccountEditorBean.java
+++ server/web/src/main/java/br/unifesp/maritaca/web/jsf/account/AccountEditorBean.java
@@ -5,9 +5,12 @@ import java.io.Serializable;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.SessionScoped;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 import br.unifesp.maritaca.core.User;
 import br.unifesp.maritaca.web.jsf.AbstractBean;
+import br.unifesp.maritaca.web.utils.Utils;
 
 /**
  * Bean responsible for handling user creation.
@@ -18,11 +21,18 @@ import br.unifesp.maritaca.web.jsf.AbstractBean;
 @SessionScoped
 public class AccountEditorBean extends AbstractBean implements Serializable{
 	
+	// TODO Use bean attributes instead of User attributes in xhtml.
+	// TODO Verify the use of Pattern in email validation and the use of 
+	//		ValidationMessages.properties
+	@Pattern(regexp = Utils.EMAIL_REG_EXP, message="{email.invalid}")
+	private String email;
+	
 	@ManagedProperty("#{currentUserBean}")
 	private CurrentUserBean currentUserBean;
 	
 	private static final long serialVersionUID = 1L;
 	private User     user;
+	
 	private String   confirmPassword;
 	
 	public AccountEditorBean() {
@@ -45,7 +55,8 @@ public class AccountEditorBean extends AbstractBean implements Serializable{
 	public String saveAccount(){
 		if(!passwordsMatch() || registeredEmail()){
 			return null;
-		}		
+		}
+		getUser().setEmail(getEmail());
 		if(!super.userCtrl.saveUser(getUser())){
 			return null;
 		}
@@ -61,7 +72,7 @@ public class AccountEditorBean extends AbstractBean implements Serializable{
 	 * @return true if the email is already taken, false otherwise.
 	 */
 	public Boolean registeredEmail(){
-		String email = getUser().getEmail();
+		String email = getEmail();
 		if(email==null || email.isEmpty()){
 			return false;
 		}
@@ -133,5 +144,13 @@ public class AccountEditorBean extends AbstractBean implements Serializable{
 
 	public void setCurrentUserBean(CurrentUserBean currentUserBean) {
 		this.currentUserBean = currentUserBean;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
 	}
 }
