524b566aba4ac9a0b538f4db0c6588ff39b4a15e
tiagobarabasz <tbarabasz@gmail.com>
2012-05-02 15:34:14 -0300

    Merge commit

--- server/maritaca-business/src/test/java/br/unifesp/maritaca/business/test/form/FormListerTest.java
+++ server/maritaca-business/src/test/java/br/unifesp/maritaca/business/test/form/FormListerTest.java
@@ -1,8 +1,5 @@
 package br.unifesp.maritaca.business.test.form;
 
-import javax.annotation.Resource;
-
-import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
