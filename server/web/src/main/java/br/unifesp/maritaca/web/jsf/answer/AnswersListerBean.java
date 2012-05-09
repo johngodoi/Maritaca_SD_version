869332d9f467e043b4f03620670adba5274222fd
tiagobarabasz <tbarabasz@gmail.com>
2012-05-09 17:22:28 -0300

    Ticket: 162, Status: Closed Exporting answers as CSV

--- server/web/src/main/java/br/unifesp/maritaca/web/jsf/answer/AnswersListerBean.java
+++ server/web/src/main/java/br/unifesp/maritaca/web/jsf/answer/AnswersListerBean.java
@@ -1,10 +1,14 @@
 package br.unifesp.maritaca.web.jsf.answer;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.inject.Inject;
 
 import br.unifesp.maritaca.business.answer.list.AnswersListerEJB;
+import br.unifesp.maritaca.business.answer.list.dto.AnswerDTO;
 import br.unifesp.maritaca.business.answer.list.dto.AnswerListerDTO;
 import br.unifesp.maritaca.business.form.dto.FormDTO;
 import br.unifesp.maritaca.web.base.MaritacaJSFBean;
@@ -14,11 +18,12 @@ import br.unifesp.maritaca.web.base.MaritacaJSFBean;
 public class AnswersListerBean extends MaritacaJSFBean {
 	private static final long serialVersionUID = 1L;
 	
+	private static final String answFileNameSufix  = "-answers.csv"; 
+	
 	@Inject
 	private AnswersListerEJB answersListerEJB;	
 
 	private AnswerListerDTO answerListerDTO;	
-	private Integer numberOfPages;
 	
 	public AnswersListerBean() {
 		/* Default constructor: Nothing here */
@@ -38,13 +43,30 @@ public class AnswersListerBean extends MaritacaJSFBean {
 	public void setAnswerListerDTO(AnswerListerDTO answerListerDTO) {
 		this.answerListerDTO = answerListerDTO;
 	}
-
-	public Integer getNumberOfPages() {
-		Integer numPages = getAnswerListerDTO()!=null?getAnswerListerDTO().getAnswers()!=null?getAnswerListerDTO().getAnswers().size():0:0;
-		return super.getNumberOfPages(numPages, getItemsPerPage());
+	
+	public String exportAnswersAsCsv() throws IOException{
+		String fileName = getAnswerListerDTO().getFormTitle() + answFileNameSufix;
+		
+		getResponse().setHeader("Content-Disposition","attachment; filename=" + fileName);  		
+		PrintWriter pr = getResponse().getWriter();
+				
+		for(AnswerDTO answDto : answerListerDTO.getAnswers()){
+			pr.print(toCsv(answDto.getUserEmail()));
+			pr.print(toCsv(answDto.getCollectDate().toString()));
+			String questions = "";
+			for(String answ : answDto.getAnswers()){
+				questions += toCsv(answ);
+			}
+			pr.print(questions.replaceAll(",$", ""));
+			pr.print("\n");
+		}
+		pr.flush();
+		pr.close();
+		
+		return null;
 	}
-
-	public void setNumberOfPages(Integer numberOfPages) {
-		this.numberOfPages = numberOfPages;
+	
+	private String toCsv(String value){
+		return "\"" + value.replace("\n", "") + "\",";
 	}
 }
