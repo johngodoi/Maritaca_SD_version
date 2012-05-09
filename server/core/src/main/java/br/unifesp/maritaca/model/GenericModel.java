deab219118188d1ea5bde19fb426fcdb9c8162f3
jimvalsan <jimvalsan@svn.code.sf.net>
2012-05-09 13:56:15 -0300

    Ticket: 161, Status: Closed Remove old model classes

--- server/core/src/main/java/br/unifesp/maritaca/model/GenericModel.java
+++
@@ -1,8 +0,0 @@
-package br.unifesp.maritaca.model;
-
-import br.unifesp.maritaca.core.User;
-
-public interface GenericModel {
-	void setCurrentUser(User currentUser);
-	User getCurrentUser();
-}
--- server/core/src/main/java/br/unifesp/maritaca/model/UseEntityManager.java
+++
@@ -1,8 +0,0 @@
-package br.unifesp.maritaca.model;
-
-import br.unifesp.maritaca.persistence.EntityManager;
-
-public interface UseEntityManager {
-
-	public EntityManager getEntityManager();
-}
